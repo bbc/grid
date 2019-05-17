d25b7af186c8b92fe170b2faf3e653fc81a7f069
Felix Stephenson <Felix.Stephenson@bbc.co.uk>
2019-05-17 10:38:51 +0100

    media-api es6 tests read env var to determine whether to use Docker in code

--- media-api/test/lib/elasticsearch/ElasticSearchTestBase.scala
+++ media-api/test/lib/elasticsearch/ElasticSearchTestBase.scala
@@ -13,15 +13,15 @@ import org.scalatest.time.{Milliseconds, Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers}
 
 import scala.concurrent.duration._
+import scala.util.Properties
 
 trait ElasticSearchTestBase extends FunSpec with BeforeAndAfterAll with Matchers with ScalaFutures with Fixtures with DockerKit with DockerTestKit with DockerKitSpotify {
 
   val interval = Interval(Span(100, Milliseconds))
   val timeout = Timeout(Span(10, Seconds))
 
-  val testProps = getTestProperties
-  val es6TestUrl = testProps.getOrElse("es6.test.url", "http://localhost:9200")
-  val useEsDocker = testProps.getOrElse("es6.useDocker", "true").toBoolean
+  val useEsDocker = Properties.envOrElse("ES6_USE_DOCKER", "true").toBoolean
+  val es6TestUrl = Properties.envOrElse("ES6_TEST_URL", "http://localhost:9200")
 
   def esContainer: Option[DockerContainer]
 
