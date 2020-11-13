5c2500625422d6db614cdca2999323d81d59f8fd
Ochieng Olanga <ochiengolanga@gmail.com>
2020-11-13 18:22:53 +0300

    add transcoded.mime.types=image/tiff in imageloader dev template

--- dev/script/generate-dot-properties/service-config.js
+++ dev/script/generate-dot-properties/service-config.js
@@ -75,6 +75,7 @@ function getImageLoaderConfig(config) {
         |s3.config.bucket=${config.coreStackProps.ConfigBucket}
         |security.cors.allowedOrigins=${getCorsAllowedOriginString(config)}
         |metrics.request.enabled=false
+        |transcoded.mime.types=image/tiff
         |`;
 }
 
