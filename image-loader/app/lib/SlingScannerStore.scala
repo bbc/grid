f39ad14c4fe6607d2a50d2636572f1b89947609d
Ochieng Olanga <ochiengolanga@gmail.com>
2020-11-27 17:42:34 +0300

    Delete VirusScanner.scala and SlingScannerStore.scala

--- image-loader/app/lib/SlingScannerStore.scala
+++
@@ -1,6 +0,0 @@
-package lib.storage
-
-import lib.ImageLoaderConfig
-import com.gu.mediaservice.lib
-
-class SlingScannerStore(config: ImageLoaderConfig) extends lib.ImageIngestOperations(config.quarantineBucket, config.thumbnailBucket, config)
