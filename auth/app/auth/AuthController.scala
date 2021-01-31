038b474891379fdb58c49a82d2dada2d73c8db47
Georges Ribeiro <georges.ribeiro.ext@bbc.co.uk>
2021-01-30 22:05:59 -0300

    Hack until redirect from PP is corrected

--- auth/app/auth/AuthController.scala
+++ auth/app/auth/AuthController.scala
@@ -29,7 +29,7 @@ class AuthController(auth: Authentication, providers: AuthenticationProviders, v
     respond(indexData, indexLinks)
   }
 
-  def index = auth { indexResponse }
+  def index = doLogin(None)
 
   def session = auth { request =>
     val showPaid = hasPermission(request.user, Permissions.ShowPaid)
