a6efcf4f89a3d40fa629d3fa01f86723baae3e00
ebuson <ebusonsd@gmail.com>
2023-03-03 19:42:14 +0200

    get pp sessin

--- kahuna/public/js/services/api/media-api.js
+++ kahuna/public/js/services/api/media-api.js
@@ -77,7 +77,10 @@ mediaApi.factory('mediaApi',
     function getPPSession(link) {
         // TODO: workout how we might be able to memoize this function but still
         // play nice with changes that might occur in the API (cache-header?).
-        return root.follow(link).getData();
+        ppSession = root.follow(link)
+        console.log("ppSession root: ", ppSession);
+        console.log("ppSession get: ", ppSession.get());
+        return ppSession.get();
     }
 
     function metadataSearch(field, { q }) {
